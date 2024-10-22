4289ca02602bdc86b6488091355e686083427a8c
Noemc211 <nmcmucru211@gmail.com>
2024-10-21 23:17:30 -0600

    ##00-Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Noemc211.py
@@ -0,0 +1,27 @@
+# https://www.python.org/
+# Esto es un texto de línea 
+
+
+"""
+Esto es texto entre lineas 
+"""
+
+'''
+Esto es otra forma de poner texto entre líneas
+'''
+
+my_variable = "Mi variable"
+my_variable = "Nuevo valor de mi variable"
+
+MY_CONSTANT = "Mi constante" # por convención 
+
+"En phyton no existen las constantes"
+
+my_int = 1
+my_float = 1.5 
+my_bool = True 
+my_bool = False 
+my_string = "Mi cadena de texto"
+my_other_string = 'Mi otra cadena de texto'
+
+print("¡Hola!, Python")
